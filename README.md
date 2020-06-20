833240d89f85286770805353aba2cf9339bc7e0c
Bao-Jiarong <bao.salirong@gmail.com>
2020-06-20 23:02:36 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,57 @@
+## LeNet Implementation
+This repository is about LeNet in Tensorflow 2.  
+I used tf.keras.Model and tf.layers.Layer instead of tf.keras.models.Sequential.  
+This allows us to customize and have full control of the model.  
+I also used custom training instead of relying on the fit() function.  
+In case we have very huge dataset, I applied online loading (by batch) instead of loading the data completely at the beginning. This will eventually not consume the memory.  
+However, in case you prefer tf.keras.models.Sequential based models and loading the data completely in the beginning, then please refer to my repository: [LeNet](https://github.com/Bao-Jiarong/LeNet)
+
+### LeNet Architecrure
+<p></p>
+<center>
+<img src="img/lenet.png" align="center" width="700" height="300"/>
+</center>
+
+image is taken from [source](https://tianhaoo.github.io/2019/05/22/LeNet-5%E8%AF%86%E5%88%AB%E6%89%8B%E5%86%99%E6%95%B0%E5%AD%97/)   
+
+<center>   
+<img src="img/1.png" width="700" height="200"/>   
+</center>
+
+image is taken from [source](https://neurohive.io/en/popular-networks/vgg16/)   
+
+### Training on MNIST
+<p></p>
+<center>
+<img src="img/mnist.png" width="400" height="350"/>
+</center>
+
+### Requirement
+```
+python==3.7.0
+numpy==1.18.1
+```
+### How to use
+Training & Prediction can be run as follows:    
+`python train.py train`  
+`python train.py predict img.png`  
+
+
+### More information
+* Please refer to the original paper of LeNet [here](http://yann.lecun.com/exdb/publis/pdf/lecun-01a.pdf) for more information.
+
+### Implementation Note
+* LeNet used 'relu' as activate function, and MaxPool2D for pooling.
+* LeNet_5 used 'tanh' as activate function, and AveragePooling2D for pooling.
+
+### Result for MNIST:   
+* Learning rate = 0.001  
+* Batch size = 32  
+* Optimizer = Adam   
+
+Name |  epochs  | Training Accuracy |  Validation Accuracy  |
+:---: | :---: | :---: | :---:
+LeNet   | 2  |  96.62% | 96.85%
+LeNet_5 | 2 |  96.54% | 96.64%
+LeNet   | 10  |  98.28% | 97.69%
+LeNet_5 | 10 |  98.82% | 98.30%
---
+++ lenet.py
@@ -0,0 +1,100 @@
+''''
+  Author       : Bao Jiarong
+  Creation Date: 2020-06-20
+  email        : bao.salirong@gmail.com
+  Task         : LeNet based on Keras Model
+ '''
+
+import tensorflow as tf
+#==========================LeNet based on Keras Model===========================
+class Block(tf.keras.layers.Layer):
+    def __init__(self, filters, kernel_size, strides = (1,1), n = 1, activation = "tanh"):
+        super(Block, self).__init__()
+        self.conv = tf.keras.layers.Conv2D(filters = filters, kernel_size = kernel_size,
+                                                     strides = strides, activation  = activation,
+                                                     padding = "valid")
+        self.pool = tf.keras.layers.AveragePooling2D(pool_size=(2,2), strides=(2,2))
+
+    def call(self, inputs, **kwargs):
+        x = inputs
+        x = self.conv(x)
+        x = self.pool(x)
+        return x
+
+class LeNet(tf.keras.Model):
+    def __init__(self, classes=None):
+        super(LeNet, self).__init__()
+
+        self.block1 = Block(6  , kernel_size=(5,5), n = 1)
+        self.block2 = Block(16 , kernel_size=(5,5), n = 1)
+        self.block3 = tf.keras.layers.Conv2D(filters = 120, kernel_size = (5,5),
+                                             strides = (1,1), activation  = "tanh",
+                                             padding = "valid")
+
+        self.flatten= tf.keras.layers.Flatten()
+        self.dense1 = tf.keras.layers.Dense(units = 84, activation = "tanh")
+        self.dense2 = tf.keras.layers.Dense(units = classes, activation="softmax")
+
+    def call(self, inputs, training = None):
+        # Backbone
+        x = inputs
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+
+        # Top
+        x = self.flatten(x)
+        x = self.dense1(x)
+        x = self.dense2(x)
+        return x
+
+#------------------------------------------------------------------------------
+def LeNet_5(input_shape, classes):
+    model = LeNet(classes)
+    model.build(input_shape = input_shape)
+    return model
+
+#==========================LeNet based on Keras Sequential======================
+'''
+class Block(tf.keras.models.Sequential):
+    def __init__(self,n):
+        super().__init__()
+        self.add(tf.keras.layers.Conv2D(filters = n, kernel_size=(5,5),strides=(1,1),padding = 'valid',activation = "tanh"))
+        self.add(tf.keras.layers.AveragePooling2D(pool_size = (2, 2)))
+
+class Block1(tf.keras.models.Sequential):
+    def __init__(self,n):
+        super().__init__()
+        self.add(tf.keras.layers.Conv2D(filters = n, kernel_size=(5,5),strides=(1,1),padding = 'valid',activation = "relu"))
+        self.add(tf.keras.layers.MaxPool2D(pool_size = (2, 2)))
+
+class LeNet_5(tf.keras.models.Sequential):
+    def __init__(self, input_shape, classes):
+        super().__init__()
+        self.add(tf.keras.layers.InputLayer(input_shape = input_shape))
+
+        # Backbone
+        self.add(Block(n = 6))
+        self.add(Block(n = 16))
+        self.add(tf.keras.layers.Conv2D(filters = 120, kernel_size=(5,5),strides=(1,1),padding = 'valid',activation = "tanh"))
+        # top
+        self.add(tf.keras.layers.Flatten())
+        self.add(tf.keras.layers.Dense(units = 84, activation = "tanh"))
+        self.add(tf.keras.layers.Dense(units = classes,activation = "softmax"))
+
+
+class LeNet(tf.keras.models.Sequential):
+    def __init__(self, input_shape, classes):
+        super().__init__()
+        self.add(tf.keras.layers.InputLayer(input_shape = input_shape))
+
+        # Backbone
+        self.add(Block1(n = 6))
+        self.add(Block1(n = 16))
+        self.add(tf.keras.layers.Conv2D(filters = 120, kernel_size=(5,5),strides=(1,1),padding = 'valid',activation = "relu"))
+
+        #top
+        self.add(tf.keras.layers.Flatten())
+        self.add(tf.keras.layers.Dense(units = 84, activation = "relu"))
+        self.add(tf.keras.layers.Dense(units = classes, activation = "softmax"))
+'''
